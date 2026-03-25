3e02f99e51776039d87bae70ff33efa6a3c3a65d
Andrew Panta <andrewpanta@mail.weber.edu>
2026-03-24 19:08:15 -0700

    Primera idea de codigo

---
+++ parte_de_andrew.py
@@ -0,0 +1,16 @@
+# Voy a crear un a funcion para hacer los calculos
+def calcular_transferencia(pais_destino, monto_usd):
+# Esto lo aprendimos en la clase pasada para poner la fecha 
+    import random
+    from datetime import datetime
+
+#Aca estaran las tasa de cambio de monedas de cada pais que elijamos, quizas con un if
+
+
+#Aca estaran los calculos del monto que el usario ingre, la tasa de cambio y el costo dl servicio
+    monto_convertido = monto_usd * tasa
+    costo_servicio = (monto_usd * 0.05)
+    total_a_pagar = monto_usd + costo_servicio
+
+#Aqui nos debe dar el ID y la fecha
+id_transferencia = random.randint(100000, 999999)
